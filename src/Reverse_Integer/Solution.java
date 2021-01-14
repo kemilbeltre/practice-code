f902c331eabfc89839763e1820d89dff1d5de701
kemilbeltre <kemilbeltre@gmail.com>
2021-01-14 22:43:46 +0000

    day 3

---
+++ src/Reverse_Integer/Solution.java
@@ -0,0 +1,38 @@
+/*
+ * Given a signed 32-bit integer x, return x with its digits reversed. 
+ * If reversing x causes the value to go outside the signed 
+ * 32-bit integer range [-231, 231 - 1], then return 0
+ */
+
+package Reverse_Integer;
+
+public class Solution {
+	
+	public static int reverse(int x) {
+		
+		boolean negative = false;
+		if (x < 0) {
+			negative = true;
+			x *= -1;
+		}
+
+		long reversed = 0;
+		while (x > 0) {
+			reversed = (reversed * 10) + (x % 10);
+			x /= 10;
+		}
+		if (reversed > Integer.MAX_VALUE) {
+			return 0;
+		}
+		return negative ? (int) (-1 * reversed) : (int) reversed;
+	}
+	
+	// Driver code 
+    public static void main (String[] args)  
+    { 
+        int x = 4562; 
+        System.out.println("Reverse of no. is " 
+                           + reverse(x)); 
+    } 
+
+}
