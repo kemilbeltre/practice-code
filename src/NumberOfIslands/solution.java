d3ba6c539709b5fb24623c7bd4d36b85879a86ae
Kemil <kemilbeltre@gmail.com>
2021-05-11 23:25:44 +0100

    Number of Islands
     exercise

---
+++ src/NumberOfIslands/solution.java
@@ -0,0 +1,27 @@
+package NumberOfIslands;
+
+public class solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i= 0; i<grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if (grid[i][j] == '1') {
+                    count += 1;
+                    callBFS(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void callBFS(char[][] grid, int i, int j) {
+        if (i < 0 || i>=grid.length || j < 0 || j>=grid[i].length || grid[i][j] == '0')
+            return;
+
+            grid[i][j] = '0';
+            callBFS(grid, i+1, j); //up
+            callBFS(grid, i-1, j);  //down
+            callBFS(grid, i, j-1); //left
+            callBFS(grid, i+1, j+1); //right
+    }
+}
